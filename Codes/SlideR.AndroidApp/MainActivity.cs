140f72e411d6c1e072ae17fdc39450c4d8d3ac30
Yiğitcan ŞENER <ycansener@gmail.com>
2016-05-17 02:08:39 +0300

    Add files via upload

---
+++ Codes/SlideR.AndroidApp/MainActivity.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SlideR
+{
+    [Activity(Label = "SlideR", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Slider.Client.Core.CMDClient client;
+        public static IPAddress GetLocalIPAddressFromHostName(string hostName)
+        {
+            var host = Dns.GetHostEntry(hostName);
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+
+            Button buttonConnect = FindViewById<Button>(Resource.Id.buttonConnect);
+
+            buttonConnect.Click += buttonConnect_Click;
+
+            Button buttonPrevious = FindViewById<Button>(Resource.Id.buttonPrevious);
+            buttonPrevious.Enabled = false;
+            buttonPrevious.Click += buttonPrevious_Click;
+
+
+            Button buttonNext = FindViewById<Button>(Resource.Id.buttonNext);
+            buttonNext.Enabled = false;
+            buttonNext.Click += buttonNext_Click;
+        }
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            EditText editTextIpAddress = FindViewById<EditText>(Resource.Id.editTextServerIp);
+            if (!string.IsNullOrEmpty(editTextIpAddress.Text))
+            {
+                string ipAddress = editTextIpAddress.Text;
+                string[] ipParts = ipAddress.Split('.');
+
+                if(ipParts.Length == 4)
+                {
+                    this.client = new Slider.Client.Core.CMDClient(IPAddress.Parse(ipAddress), 8000, "None");
+                    client.ConnectToServer();
+                    button.Enabled = false;
+
+                    Button buttonNext = FindViewById<Button>(Resource.Id.buttonNext);
+                    buttonNext.Enabled = true;
+                    Button buttonPrevious = FindViewById<Button>(Resource.Id.buttonPrevious);
+                    buttonPrevious.Enabled = true;
+                }
+                else
+                {
+                    Toast.MakeText(this.ApplicationContext, "Invalid host ip!", ToastLength.Short);
+                }
+            }
+            else
+            {
+                Toast.MakeText(this.ApplicationContext, "Invalid host ip!", ToastLength.Short);
+            }
+        }
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            if (this.client.Connected)
+            {
+                this.client.SendCommand(new Slider.Client.Core.Command(Slider.Client.Core.CommandType.Message, System.Net.IPAddress.Broadcast, "prev"));
+            }
+        }
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (this.client.Connected)
+            {
+                this.client.SendCommand(new Slider.Client.Core.Command(Slider.Client.Core.CommandType.Message, System.Net.IPAddress.Broadcast, "next"));
+            }
+        }
+    }
+}
+
---
+++ Codes/SlideR.HostApp/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Slider.Server.Core;
+
+namespace Slider.HostApp
+{
+    class Program
+    {
+        private System.Collections.Generic.List<ClientManager> clients;
+        private BackgroundWorker bwListener;
+        private Socket listenerSocket;
+        private IPAddress serverIP;
+        private int serverPort;
+
+        /// <summary>
+        /// Start the console server.
+        /// </summary>
+        /// <param name="args">These are optional arguments.Pass the local ip address of the server as the first argument and the local port as the second argument.</param>
+        static void Main(string[] args)
+        {
+            Program progDomain = new Program();
+            progDomain.clients = new List<ClientManager>();
+
+            progDomain.serverPort = 8000;
+            //progDomain.serverIP = IPAddress.Any;
+            progDomain.serverIP = GetLocalIPAddress();
+
+            progDomain.bwListener = new BackgroundWorker();
+            progDomain.bwListener.WorkerSupportsCancellation = true;
+            progDomain.bwListener.DoWork += new DoWorkEventHandler(progDomain.StartToListen);
+            progDomain.bwListener.RunWorkerAsync();
+
+            Console.WriteLine("*** Listening on port {0}{1}{2} started.Press ENTER to shutdown server. ***\n", progDomain.serverIP.ToString(), ":", progDomain.serverPort.ToString());
+
+            Console.ReadLine();
+
+            progDomain.DisconnectServer();
+        }
+        public static IPAddress GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+
+        private void StartToListen(object sender, DoWorkEventArgs e)
+        {
+            this.listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            this.listenerSocket.Bind(new IPEndPoint(this.serverIP, this.serverPort));
+            this.listenerSocket.Listen(200);
+            while (true)
+                this.CreateNewClientManager(this.listenerSocket.Accept());
+        }
+        private void CreateNewClientManager(Socket socket)
+        {
+            ClientManager newClientManager = new ClientManager(socket);
+            newClientManager.CommandReceived += new CommandReceivedEventHandler(CommandReceived);
+            newClientManager.Disconnected += new DisconnectedEventHandler(ClientDisconnected);
+            this.CheckForAbnormalDC(newClientManager);
+            this.clients.Add(newClientManager);
+            this.UpdateConsole("Connected.", newClientManager.IP, newClientManager.Port);
+        }
+
+        private void CheckForAbnormalDC(ClientManager mngr)
+        {
+            if (this.RemoveClientManager(mngr.IP))
+                this.UpdateConsole("Disconnected.", mngr.IP, mngr.Port);
+        }
+
+        void ClientDisconnected(object sender, ClientEventArgs e)
+        {
+            if (this.RemoveClientManager(e.IP))
+                this.UpdateConsole("Disconnected.", e.IP, e.Port);
+        }
+
+        private bool RemoveClientManager(IPAddress ip)
+        {
+            lock (this)
+            {
+                int index = this.IndexOfClient(ip);
+                if (index != -1)
+                {
+                    string name = this.clients[index].ClientName;
+                    this.clients.RemoveAt(index);
+
+                    //Inform all clients that a client had been disconnected.
+                    Command cmd = new Command(CommandType.ClientLogOffInform, IPAddress.Broadcast);
+                    cmd.SenderName = name;
+                    cmd.SenderIP = ip;
+                    this.BroadCastCommand(cmd);
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        private int IndexOfClient(IPAddress ip)
+        {
+            int index = -1;
+            foreach (ClientManager cMngr in this.clients)
+            {
+                index++;
+                if (cMngr.IP.Equals(ip))
+                    return index;
+            }
+            return -1;
+        }
+
+        private void CommandReceived(object sender, CommandEventArgs e)
+        {
+            if (e.Command.MetaData.Equals("next"))
+            {
+                SendKeys.SendWait("{RIGHT}");
+            }
+            else if (e.Command.MetaData.Equals("prev"))
+            {
+                SendKeys.SendWait("{LEFT}");
+            }
+
+            this.SendCommandToTarget(e.Command);
+        }
+
+        private void BroadCastCommand(Command cmd)
+        {
+            foreach (ClientManager mngr in this.clients)
+                if (!mngr.IP.Equals(cmd.SenderIP))
+                    mngr.SendCommand(cmd);
+        }
+
+        private void SendCommandToTarget(Command cmd)
+        {
+            Console.WriteLine(cmd.SenderName + ": " + cmd.MetaData);
+        }
+        private void UpdateConsole(string status, IPAddress IP, int port)
+        {
+            Console.WriteLine("Client {0}{1}{2} has been {3} ( {4}|{5} )", IP.ToString(), ":", port.ToString(), status, DateTime.Now.ToShortDateString(), DateTime.Now.ToLongTimeString());
+        }
+        public void DisconnectServer()
+        {
+            if (this.clients != null)
+            {
+                foreach (ClientManager mngr in this.clients)
+                    mngr.Disconnect();
+
+                this.bwListener.CancelAsync();
+                this.bwListener.Dispose();
+                this.listenerSocket.Close();
+                GC.Collect();
+            }
+        }
+    }
+}
